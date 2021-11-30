481f99955763ad0667d6b70fa66d2da468fb5da7
srikarprapulla <n01479916@humber.ca>
2021-11-29 23:56:48 -0500

    added javascript file for bowling page

---
+++ js/bowling.js
@@ -0,0 +1,33 @@
+function myFunction() {
+    alert (" this page will be redirected to home page you can select your slot in home page ");
+    window.location.href = "index.html";
+}
+
+function mouseoverevent()
+	{
+        
+        console.log("mouse funct");
+        console.log("after effect",x,y);
+        document.getElementsByClassName("col1")[0].style.backgroundColor="blue";
+        document.getElementsByClassName("col2")[0].style.backgroundColor="red";
+        document.getElementsByClassName("col3")[0].style.backgroundColor="brown";
+        document.getElementsByClassName("col4")[0].style.backgroundColor="grey";
+        
+	}
+
+//document.getElementById("nav_menu").addEventListener("mouseover", function() {
+//    document.getElementById("nav_menu").style.backgroundColor = "red";
+//});
+    
+//document.getElementById("nav_menu").addEventListener("mouseout", function() {
+//    document.getElementById("nav_menu").style.backgroundColor = "black";
+//});
+
+//document.getElementsByTagName("li").onmouseover.="yellow";
+
+
+
+
+//document.getElementsByTagName("li").onmouseout.fontcolor = "black";
+
+    

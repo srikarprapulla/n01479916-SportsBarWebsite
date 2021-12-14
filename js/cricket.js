37be8f6d7a7ba2be42a761acff329d5e5b53a3dc
rana0909 <ranapratapedara@gmail.com>
2021-12-14 15:46:28 -0500

    updated path for json cricket.

--- js/cricket.js
+++ js/cricket.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.getJSON("./../json/cricket.json", function (data) {
+  $.getJSON("../json/cricket.json", function (data) {
     $.each(data, function (index, eachObj) {
       if (index % 2 == 1) {
         $("#text").append(
